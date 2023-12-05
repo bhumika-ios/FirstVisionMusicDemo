7b723926e908788a42f2d223836cfe9a9a96a2f9
bhumika-ios <bhumika.iosdeveloper@gmail.com>
2023-12-05 16:02:10 +0530

    create model group

---
+++ FirstVisionMusicDemo/Model/SideMenuItem.swift
@@ -0,0 +1,18 @@
+//
+//  SideMenuItem.swift
+//  FirstVisionMusicDemo
+//
+//  Created by Bhumika Patel on 05/12/23.
+//
+
+import SwiftUI
+
+struct SideMenuItem: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SideMenuItem()
+}
