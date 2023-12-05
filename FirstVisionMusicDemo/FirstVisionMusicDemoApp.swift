66f9584a2904186d2209c18a59523fd376d15f67
bhumika-ios <bhumika.iosdeveloper@gmail.com>
2023-12-05 15:35:50 +0530

    Initial Commit

---
+++ FirstVisionMusicDemo/FirstVisionMusicDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstVisionMusicDemoApp.swift
+//  FirstVisionMusicDemo
+//
+//  Created by Bhumika Patel on 05/12/23.
+//
+
+import SwiftUI
+
+@main
+struct FirstVisionMusicDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
