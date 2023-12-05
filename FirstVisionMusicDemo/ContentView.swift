db0e538c26e188154b9808002b77591a521f2cd2
bhumika-ios <bhumika.iosdeveloper@gmail.com>
2023-12-05 15:41:26 +0530

    create navigationsplitview for used menu item

--- FirstVisionMusicDemo/ContentView.swift
+++ FirstVisionMusicDemo/ContentView.swift
@@ -11,13 +11,13 @@ import RealityKitContent
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Model3D(named: "Scene", bundle: realityKitContentBundle)
-                .padding(.bottom, 50)
-
-            Text("Hello, world!")
+        NavigationSplitView{
+            List {
+                Text("Menu Item")
+            }
+        } detail: {
+            
         }
-        .padding()
     }
 }
 
